8cacf91be036b4203c3b4b99cef2993efe24bae0
AaLlice <3428986827@qq.com>
2020-05-02 21:47:18 +0800

    维修人员

--- After_Sales/src/main/java/com/zgl/aftersales/controller/WorkerController.java
+++ After_Sales/src/main/java/com/zgl/aftersales/controller/WorkerController.java
@@ -33,9 +33,8 @@ public class WorkerController {
     根据user_id来显示维修人员的信息
     测试时需先登录
      */
-    @RequestMapping("/worker_selectBy_Session_UserId")
+    @RequestMapping(value = "/worker_selectBy_Session_UserId",method = RequestMethod.GET)
     public Users worker_selectBy_Session_UserId(HttpServletRequest req){
-        DesDecodeUtiles desDecodeUtiles=new DesDecodeUtiles();
 
         //将登录的session的User_id取出来
         int User_id= (int) req.getSession(true).getAttribute("userID");
@@ -43,7 +42,7 @@ public class WorkerController {
         Users user=workerService.worker_selectBy_Session_UserId(User_id);
 
         //密码解密之后输出
-        user.setPassword(desDecodeUtiles.getDecryptString(user.getPassword()));
+        user.setPassword(DesDecodeUtiles.getDecryptString(user.getPassword()));
         return user;
     }
 
@@ -56,7 +55,6 @@ public class WorkerController {
      */
     @PostMapping("/worker_updateBy_Session_UserId")
     public Map<String, Object> worker_updateBy_Session_UserId(@RequestBody JSONObject json, HttpServletRequest req) {
-        DesDecodeUtiles desDecodeUtiles = new DesDecodeUtiles();
 
         //将登录的session的User_id取出来
         int User_id = (int) req.getSession(true).getAttribute("userID");
@@ -94,7 +92,7 @@ public class WorkerController {
                         try {
                             map.put("User_id",User_id);
                             map.put("User_name",json.getString("User_name"));
-                            map.put("Password",desDecodeUtiles.getEncryptString(json.getString("Password")));
+                            map.put("Password", DesDecodeUtiles.getEncryptString(json.getString("Password")));
                             map.put("Tel",json.getString("Tel"));
                             map.put("Email",json.getString("Email"));
                             map.put("isupdate","修改成功");
@@ -102,7 +100,7 @@ public class WorkerController {
                         } catch (Exception e) {
                             map.put("User_id",User_id);
                             map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
-                            map.put("Password",desDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
+                            map.put("Password", DesDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
                             map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                             map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
                             map.put("isupdate","修改失败,该用户名已存在");
@@ -110,7 +108,7 @@ public class WorkerController {
                     } else {
                         map.put("User_id",User_id);
                         map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
-                        map.put("Password",desDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
+                        map.put("Password", DesDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
                         map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                         map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
                         map.put("isupdate","修改邮箱失败，请输入正确邮箱");
@@ -118,7 +116,7 @@ public class WorkerController {
                 } else {
                     map.put("User_id",User_id);
                     map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
-                    map.put("Password",desDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
+                    map.put("Password", DesDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
                     map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                     map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
                     map.put("isupdate","修改电话号码失败，请输入正确的电话号码");
@@ -126,7 +124,7 @@ public class WorkerController {
             } else {
                 map.put("User_id",User_id);
                 map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
-                map.put("Password",desDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
+                map.put("Password", DesDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
                 map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                 map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
                 map.put("isupdate","修改密码失败，密码格式错误，必须由数字和字母组成，且长度大于6");
@@ -134,7 +132,7 @@ public class WorkerController {
         }else {
             map.put("User_id",User_id);
             map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
-            map.put("Password",desDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
+            map.put("Password", DesDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
             map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
             map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
             map.put("isupdate","修改失败，用户名不符合格式，不能全为数字，可以包含下划线");
