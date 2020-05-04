d3cbe4b951e38afc6ca1e84e495854a470ec4574
AaLlice <3428986827@qq.com>
2020-05-04 10:10:08 +0800

    用户不能查询密码，不能修改密码

--- After_Sales/src/main/java/com/zgl/aftersales/controller/WorkerController.java
+++ After_Sales/src/main/java/com/zgl/aftersales/controller/WorkerController.java
@@ -38,20 +38,28 @@ public class WorkerController {
      * login之后会产生一个session，session里面保存的为当前登录的user_id,
      *      根据user_id来显示维修人员的信息
      *      测试时需先登录
+     *      查询时不能查询出密码
      * @param req
      * @return
      */
     @RequestMapping(value = "/worker_selectBy_Session_UserId",method = RequestMethod.GET)
-    public Users worker_selectBy_Session_UserId(HttpServletRequest req){
+    public Map<String, Object> worker_selectBy_Session_UserId(HttpServletRequest req){
 
-       //将登录的session的User_id取出来
         int User_id= (int) req.getSession(false).getAttribute("userID");
-
-        Users user=workerService.worker_selectBy_Session_UserId(User_id);
-
-        //密码解密之后输出
-        user.setPassword(DesDecodeUtiles.getDecryptString(user.getPassword()));
-        return user;
+        Map<String, Object> map = new HashMap<String, Object>();
+        try {
+            map.put("User_id",User_id);
+            map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
+            map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
+            map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
+            map.put("Task_num",workerService.worker_selectBy_Session_UserId(User_id).getTask_num());
+            map.put("Role_id",workerService.worker_selectBy_Session_UserId(User_id).getRole_id());
+            map.put("Item_id",workerService.worker_selectBy_Session_UserId(User_id).getItem_id());
+            map.put("status","查询成功");
+        }catch (Exception e){
+            map.put("status","查询失败");
+        }
+        return map;
     }
 
 
@@ -71,14 +79,9 @@ public class WorkerController {
 
         Map<String, Object> map = new HashMap<String, Object>();
 
-        //修改信息时要注意是否满足要求
-
-        //从前端输入的username
         String username = json.getString("User_name");
-//        String password = json.getString("Password");
         String tel = json.getString("Tel");
         String email = json.getString("Email");
-
 
         /**
          * 验证手机号码
@@ -91,13 +94,10 @@ public class WorkerController {
          * @return
          */
 
-        //不能全为数字，可以包含下划线
-        String patternUserName = "^(?!\\d+$)[\\da-zA-Z_\\u4E00-\\u9FA5]+$";
-        //必须由数字和字母组成，且长度大于6
-//        String patternPwd = "^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,}$";
+
         String patternTel = "^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\\d{8}$";
         String patternMail = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";
-        if (Pattern.matches(patternUserName, username) && !username.equals("")) {
+        if (!username.equals("")) {
             if (Pattern.matches(patternTel, tel) && !tel.equals("")) {
                 if (Pattern.matches(patternMail, email) && !email.equals("")) {
                     try {
@@ -105,28 +105,28 @@ public class WorkerController {
                         map.put("User_name",json.getString("User_name"));
                         map.put("Tel",json.getString("Tel"));
                         map.put("Email",json.getString("Email"));
-                        map.put("isupdate","修改成功");
+                        map.put("status","修改成功");
                         workerService.worker_updateBy_Session_UserId(map);
                     } catch (Exception e) {
                         map.put("User_id",User_id);
                         map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
                         map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                         map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
-                        map.put("isupdate","修改失败,该用户名已存在");
+                        map.put("status","修改失败,该用户名已存在");
                     }
                 } else {
                     map.put("User_id",User_id);
                     map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
                     map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                     map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
-                    map.put("isupdate","修改邮箱失败，请输入正确邮箱");
+                    map.put("status","修改邮箱失败，请输入正确邮箱");
                 }
             } else {
                 map.put("User_id",User_id);
                 map.put("User_name",workerService.worker_selectBy_Session_UserId(User_id).getUser_name());
                 map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
                 map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
-                map.put("isupdate","修改电话号码失败，请输入正确的电话号码");
+                map.put("status","修改电话号码失败，请输入正确的电话号码");
             }
         }else {
             map.put("User_id",User_id);
@@ -134,7 +134,7 @@ public class WorkerController {
             map.put("Password", DesDecodeUtiles.getEncryptString(workerService.worker_selectBy_Session_UserId(User_id).getPassword()));
             map.put("Tel",workerService.worker_selectBy_Session_UserId(User_id).getTel());
             map.put("Email",workerService.worker_selectBy_Session_UserId(User_id).getEmail());
-            map.put("isupdate","修改失败，用户名不符合格式，不能全为数字，可以包含下划线");
+            map.put("status","修改失败，用户名不能为空");
         }
        return map;
     }
