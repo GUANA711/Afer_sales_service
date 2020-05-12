70b5ee99ee0627bea8758a79bb1d75fa2065e545
jumperjqw <394319168@qq.com>
2020-05-12 22:37:39 +0800

    总路径

--- After_Sales/src/main/java/com/zgl/aftersales/controller/QuestionController.java
+++ After_Sales/src/main/java/com/zgl/aftersales/controller/QuestionController.java
@@ -1,9 +1,11 @@
 package com.zgl.aftersales.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.zgl.aftersales.dao.MyLog;
 import com.zgl.aftersales.pojo.Question;
 import com.zgl.aftersales.pojo.Users;
 import com.zgl.aftersales.service.QuestionService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
@@ -11,99 +13,92 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin
 @RestController   //控制器注解
 @RequestMapping(value ="/question")
+@Slf4j
+@ResponseBody
 
+/**
+ * 问题处理类（增、查）
+ * @author jqw
+ */
 public class QuestionController {
     @Qualifier("questionImpl")
     @Autowired
     QuestionService db;
 
-    @RequestMapping("/web")  //路径注解
-    public String web() {
-        return "Hello Spring Boot!";   //显示文字内容
-    }
-
+    /**
+     * 添加问题
+     * @param json
+     * @return
+     */
     @MyLog(value = "添加问题到数据库")
     @PostMapping("/addQuestion")
-    public int addQuestion(@RequestBody Question question) {
-        return db.addQuestion(question);
-    }
+    public int addQuestion(@RequestBody JSONObject json,HttpServletRequest req) {
+        Question question = new Question();
+        Date date=new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        String date1=formatter.format(date).toString();
+        System.out.println(formatter.format(date).toString());
 
-    @GetMapping("/checkQuestion")
-    public Question checkQuestion(int Question_id) {
-        return db.checkQuestion(Question_id);
-    }
+        int User_id = (int) req.getSession(false).getAttribute("userID");
 
+        question.setItem_id(json.getInteger("item_id"));
+        question.setQuestion_detail(json.getString("question_detail"));
+        question.setQuestion_status("unaccepted");
+        question.setCommit_time(date1);
+        question.setUser_id(User_id);
+        question.setQuestion_type(json.getString("question_type"));
 
-
-//    @GetMapping("/checkQuestionsubmited")
-//    public List<Question> checkQuestionsubmited(Integer User_id) {
-//        return db.checkQuestionsubmited(User_id);
-//    }
-//
-//
-//    @GetMapping("/checkQuestionfinished")
-//    public List<Question> checkQuestionfinished(Integer User_id) {
-//        return db.checkQuestionfinished(User_id);
-//    }
-//
-//    @GetMapping("/checkQuestiondealing")
-//    public List<Question> checkQuestiondealing(Integer User_id) {
-//        return db.checkQuestiondealing(User_id);
-//    }
-//
-//
-//    @GetMapping("/checkPostMan")
-//    public Users checkPostMan(int User_id) {
-//        return db.checkPostMan(User_id);
-//    }
-
-
-    @GetMapping("/tsetin")
-    public void testin(HttpServletRequest req){
-        HttpSession session=req.getSession();
-        session.setAttribute("userID","25");
-        return;
-    }
-
-    @GetMapping("/tsetout")
-    public String testout(HttpServletRequest req,String key) {
-        String mess=(String)req.getSession().getAttribute(key);
-        return mess;
+        try {
+            db.addQuestion(question);
+        }catch (Exception e){
+            return 0;//插入失败
+        }
+        return 1;//插入成功
     }
 
 
-
+    /**
+     * 查询已提交的问题问题
+     * 问题status：unaccepted
+     */
     @GetMapping("/checkQuestionsubmited")
     public List<Question> checkQuestionsubmited(HttpServletRequest req) {
         return db.checkQuestionsubmited(Integer.parseInt(req.getSession(false).getAttribute("userID").toString()));
     }
 
-
+    /**
+     * 查询已处理完成的问题问题
+     * 问题status：done
+     */
     @GetMapping("/checkQuestionfinished")
     public List<Question> checkQuestionfinished(HttpServletRequest req) {
         return db.checkQuestionfinished(Integer.parseInt(req.getSession(false).getAttribute("userID").toString()));
     }
 
+    /**
+     * 查询正在处理的问题问题
+     * 问题status：accepted
+     */
     @GetMapping("/checkQuestiondealing")
     public List<Question> checkQuestiondealing(HttpServletRequest req) {
         return db.checkQuestiondealing(Integer.parseInt(req.getSession(false).getAttribute("userID").toString()));
     }
 
 
+    /**
+     * 查询用户个人信息
+     */
     @GetMapping("/checkPostMan")
     public Users checkPostMan(HttpServletRequest req) {
         return db.checkPostMan(Integer.parseInt(req.getSession(false).getAttribute("userID").toString()));
     }
 
 
-    @MyLog(value = "修改数据库中用户")
-    @GetMapping("/updateUser")
-    public int updateUser(List<Users> list) {
-        return db.updateUser(list);
-    }
 }
